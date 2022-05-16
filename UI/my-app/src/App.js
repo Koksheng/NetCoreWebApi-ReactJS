5a01c88e9c77f3d53f0fa309c7049f0b5aebc077
Koksheng <koksheng0406@gmail.com>
2022-05-16 12:04:22 +0800

    add UI

---
+++ UI/my-app/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import {Department} from './Department';
+import {Employee} from './Employee';
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App container">
+      <h3 className='d-flex justify-content-center m-3'>
+        React JS Frontend
+      </h3>
+
+      <nav className='navbar navbar-expand-sm bg-light navbar-dark'>
+        <ul className='navbar-nav'>
+          <li className='nav-item- m-1'>
+            <NavLink className='btn btn-light btn-outline-primary' to='/Home'>
+              Home
+            </NavLink>
+          </li>
+          <li className='nav-item- m-1'>
+            <NavLink className='btn btn-light btn-outline-primary' to='/Department'>
+              Department
+            </NavLink>
+          </li>
+          <li className='nav-item- m-1'>
+            <NavLink className='btn btn-light btn-outline-primary' to='/Employee'>
+              Employee
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/department' element={<Department/>}></Route>
+        <Route path='/employee' element={<Employee/>}></Route>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
